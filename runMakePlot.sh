f9236373e18382e3e233eda0a556a72eba4397b5
Cristovao Vilela <cvilela@lxplus736.cern.ch>
2022-08-05 01:38:29 +0200

    And remove from local dir

--- runMakePlot.sh
+++ runMakePlot.sh
@@ -3,4 +3,5 @@
 k5start -f ~/.Authentication/cvilela.kt -u cvilela
 python3 makeSummaryPlot.py --lumi_path /eos/user/c/cvilela/sndlumi_2/
 cp sndlhc_lumi_*.p* /eos/user/c/cvilela/SND-LHC/CommissioningPlots/Lumi/
+rm sndlhc_lumi_*.p*
 kdestroy -p cvilela@CERN.CH
