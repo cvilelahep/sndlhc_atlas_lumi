a8a5a974d6a8364f9b76be8fa99c2c8dd15b2284
Cristovao Vilela <cvilela@lxplus736.cern.ch>
2022-08-05 01:11:56 +0200

    update lumi tools

--- lumi_tools.py
+++ lumi_tools.py
@@ -5,6 +5,6 @@ def getLumi(lumi_path) :
     lumi_files = glob.glob(lumi_path+"/sndlhc_atlas_lumi_*.csv")
     lumi = []
     for lumi_file in lumi_files :
-        lumi.append(np.genfromtxt(lumi_file, delimiter=',', skip_header=1, dtype=[('timestamp', 'datetime64[ms]'), ('seconds_since_start', 'f8'), ('inst_lumi', 'f4'), ('integrated_lumi', 'f4')]))
+        lumi.append(np.genfromtxt(lumi_file, delimiter=',', skip_header=1, dtype=[('timestamp', 'datetime64[ms]'),  ('inst_lumi', 'f4')]))
     lumi = np.concatenate(lumi)
     return lumi
