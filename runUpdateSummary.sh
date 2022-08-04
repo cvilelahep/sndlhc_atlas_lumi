782a7303ce79cd56c7e88601050ac62e498b3638
Cristovao Vilela <cvilela@lxplus736.cern.ch>
2022-08-05 01:20:18 +0200

    Shell script wrap

---
+++ runUpdateSummary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if [[ -z run_summary.csv ]]
+then
+    cp run_summary.csv.in run_summary.csv
+fi
+
+k5start -f ~/.Authentication/cvilela.kt -u cvilela
+python3 update_run_summary.py -f run_summary.csv --lumi_path /eos/user/c/cvilela/sndlumi_2/
+kdestroy -p cvilela@CERN.CH
