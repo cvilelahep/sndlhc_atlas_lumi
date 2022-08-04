21ef7d0281f75ad2d1769b33c1a26d7a81e27005
Cristovao Vilela <cvilela@lxplus736.cern.ch>
2022-08-05 01:38:04 +0200

    Copy plots to dir

--- runMakePlot.sh
+++ runMakePlot.sh
@@ -2,4 +2,5 @@
 
 k5start -f ~/.Authentication/cvilela.kt -u cvilela
 python3 makeSummaryPlot.py --lumi_path /eos/user/c/cvilela/sndlumi_2/
+cp sndlhc_lumi_*.p* /eos/user/c/cvilela/SND-LHC/CommissioningPlots/Lumi/
 kdestroy -p cvilela@CERN.CH
