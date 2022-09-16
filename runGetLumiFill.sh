658497e6911c90435e679ab04e46fe030d9d02b9
Cris <c.vilela@cern.ch>
2022-09-16 19:27:09 +0200

    One more bug

--- runGetLumiFill.sh
+++ runGetLumiFill.sh
@@ -2,8 +2,8 @@
 
 source /cvmfs/sndlhc.cern.ch/SNDLHC-2022/July14/setUp.sh
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LOG_DIR=${SCRIPT_DIR}/Logs/
-
 LOG_FILE_NAME=${LOG_DIR}/get_nxcals_data_`date +'%Y%m%d'`.log
 
 python3 getLumiFill.py -o /eos/user/c/cvilela/nxcals_data 2>&1 | tee -a $LOG_FILE_NAME
